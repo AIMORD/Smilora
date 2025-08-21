b999f4c572274b25a4c543057a18443ccda703e7
dragonpaily <sarathsa98765@gmail.com>
2025-08-21 14:21:46 +0530

    Training using YOLO_s

--- scripts/train.py
+++ scripts/train.py
@@ -2,17 +2,16 @@ import torch
 from ultralytics import YOLO
 
 # Load a pretrained model to start a new training run
-model = YOLO('yolov8n-seg.pt')
+model = YOLO('yolov8s-seg.pt')
 
 # Start the training with performance-enhancing changes
 results = model.train(
     data='dental_dataset.yaml',
-    epochs=20,
+    epochs=150,
     imgsz=640,
     project='Dental_YOLO_Training',
     device=0,
-    name='V2(run_with_aug)', 
+    name='V3(run_with_aug_yolo_small)', 
     cls=1.5,
-    copy_paste=.2
-
+    copy_paste=0.2
 )
